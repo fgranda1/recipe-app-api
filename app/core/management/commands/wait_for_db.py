453463c0929e79ec9f9e62b42fd061d8b09c8dab
Luis Granda <searchatlas@MacBook-Pro-de-Felipe.local>
2025-04-21 17:45:07 -0500

    following the course

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+Django commanfdo to wait for a database to be avaiolable
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
---
+++ app/core/tests/tests_commands.py
@@ -0,0 +1,9 @@
+## Mock Behavior, simulute response db
+from unittest.mock import patch
+
+#Operational error to simulate a posible response error.
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
