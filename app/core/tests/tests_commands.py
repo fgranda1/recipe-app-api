f0cf7a42f72f379135d902169594414125942f93
Luis Granda <searchatlas@MacBook-Pro-de-Felipe.local>
2025-04-22 18:10:50 -0500

    adding tests commands

--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 #Operational error to simulate a posible response error.
 from psycopg2 import OperationalError as Psycopg2Error
 
+# Provided by django to call a command by name
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
